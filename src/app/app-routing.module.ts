ef02102054ef347ce0c98dbdc3c46aa86ed9a099
Brahian <bacg20044@gmail.com>
2021-09-22 13:21:34 -0500

    ADD: Auth guard for main route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -20,6 +21,7 @@ const routes: Routes = [
       import('./components/public/main-page/main-page.module').then(
         (m) => m.MainPageModule
       ),
+    canActivate: [AuthGuard],
   },
   {
     path: '**',
