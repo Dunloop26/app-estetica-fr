bc2ff6e634237e2a7a020aeca94773852c374260
Dunas26 <jhonjamesillo@gmail.com>
2021-09-30 10:46:07 -0500

    UPDATE: Login using auth service

--- src/app/components/public/sign-in-form/sign-in-form.component.ts
+++ src/app/components/public/sign-in-form/sign-in-form.component.ts
@@ -1,25 +1,38 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { IUser } from 'src/app/interfaces/user.interface';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-sign-in-form',
   templateUrl: './sign-in-form.component.html',
-  styleUrls: ['./sign-in-form.component.scss']
+  styleUrls: ['./sign-in-form.component.scss'],
 })
 export class SignInFormComponent implements OnInit {
+  constructor(private fb: FormBuilder, private auth: AuthService) {}
 
-  constructor(private fb: FormBuilder) { }
+  user: IUser = {
+    email: '',
+    password: '',
+  };
 
-  form : FormGroup = this.fb.group({
-    user: ['', [Validators.required]],
-    pass: ['', Validators.required]
+  form: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
   });
 
-  onSubmit() : void {
-    console.log(this.form.value)
+  onSubmit(): void {
+    this.auth.signIn(this.user).subscribe(
+      (data) => {
+        console.log('Hello world');
+      },
+      (error: HttpErrorResponse) => {
+        console.error('ERROR OCURRED:', error);
+      }
+    );
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,12 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { filter } from 'rxjs/operators';
+import { IUser } from '../interfaces/user.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   constructor(private http: HttpClient) {}
+
+  signIn(user: IUser): Observable<IUser | any> {
+    return this.http.post<IUser | any>('signin', JSON.stringify(user), {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
 
   isLoggedIn(): boolean {
     return false;
