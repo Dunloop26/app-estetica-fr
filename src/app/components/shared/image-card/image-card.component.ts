8912589198e9427c858257c2904dbc641a7c2368
Dunas26 <jhonjamesillo@gmail.com>
2021-10-01 17:29:31 -0500

    NEW: Image Card Component

---
+++ src/app/components/shared/image-card/image-card.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-card',
+  templateUrl: './image-card.component.html',
+  styleUrls: ['./image-card.component.scss']
+})
+export class ImageCardComponent implements OnInit {
+
+  @Input() title : string = "Title";
+  @Input() subtitle: string = "Subtitle";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onCardClick() {
+    console.log('Clicked')
+  }
+}
--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { SelectOptionComponent } from './select-option/select-option.component';
+import { ImageCardComponent } from './image-card/image-card.component';
 
 @NgModule({
-  declarations: [HeaderComponent, SelectOptionComponent],
+  declarations: [HeaderComponent, SelectOptionComponent, ImageCardComponent],
   imports: [CommonModule],
-  exports: [HeaderComponent, SelectOptionComponent],
+  exports: [HeaderComponent, SelectOptionComponent, ImageCardComponent],
 })
 export class SharedModule {}
