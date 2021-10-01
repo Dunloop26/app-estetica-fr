41b3121bd9395dcc190859356a17fe8af6ba8870
Dunas26 <jhonjamesillo@gmail.com>
2021-10-01 12:53:06 -0500

    UPDATE: Select option component styles

--- src/app/components/shared/select-option/select-option.component.ts
+++ src/app/components/shared/select-option/select-option.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 
 @Component({
   selector: 'app-select-option',
@@ -9,7 +9,21 @@ export class SelectOptionComponent implements OnInit {
 
   constructor() { }
 
+  @Output() backClicked = new EventEmitter<void>();
+  @Output() selectClicked = new EventEmitter<void>();
+
+  @Input() subtitle = "subtitle";
+  @Input() title = "title";
+
   ngOnInit(): void {
+  }
+
+  onBackButtonClicked() {
+    this.backClicked.emit();
+  }
+
+  onSelectButtonClicked() {
+    this.selectClicked.emit();
   }
 
 }
